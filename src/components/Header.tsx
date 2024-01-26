dfd924ee19ed60f6035fd01615e152b52a5907e4
kedu-3d <kduchon@student.tgm.ac.at>
2024-01-26 08:57:50 +0100

    Bug Fix Suchleiste onClick

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -68,6 +68,9 @@ export default function Navbar({
           sx={{
             alignSelf: "center",
           }}
+          onClick={() => {
+            setModalVisibility();
+          }}
         >
           <SearchRounded />
         </IconButton>
